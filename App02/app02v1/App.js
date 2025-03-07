d78027f983cfb2cc36121015fcd6d67129b281ce
Rafael Michalewicz Rodrigues <117152986+Michalewicz@users.noreply.github.com>
2025-03-07 18:14:10 -0300

    Aula03 - App2 - Adição de um componente texto pra servir como título.

--- App02/app02v1/App.js
+++ App02/app02v1/App.js
@@ -2,10 +2,12 @@
 import {View} from 'react-native'
 
 import Botao from './src/button'
+import Texto from './src/texto'
 
 function App(){
   return(
   <View>
+    <Texto nome = {'Contador de Pessoas'}/>
     <Botao nome1 = {'+'} cor1 = {'darkblue'} nome2 = {'-'} cor2 = {'darkred'}/>
   </View>
   )
---
+++ App02/app02v1/src/texto/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+
+function Texto(props){
+  return(
+    <View>
+      <Text style={{fontSize: 35, textAlign: 'center'}}>{props.nome}</Text>
+    </View>
+    )
+}
+
+export default Texto;
